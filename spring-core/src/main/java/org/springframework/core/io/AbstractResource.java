31f6bcf48df0f58799b9f631cb040bca3636ed9e
agp <360122700@qq.com>
2019-02-13 16:29:37 +0800

    修改

--- spring-core/src/main/java/org/springframework/core/io/AbstractResource.java
+++ spring-core/src/main/java/org/springframework/core/io/AbstractResource.java
@@ -44,39 +44,36 @@ import org.springframework.util.ResourceUtils;
 public abstract class AbstractResource implements Resource {
 
 	/**
-	 * This implementation checks whether a File can be opened,
-	 * falling back to whether an InputStream can be opened.
-	 * This will cover both directories and content resources.
+	 * 判断文件是否存在，若判断过程产生异常（因为会调用SecurityManager来判断），就关闭对应的流
 	 */
 	@Override
 	public boolean exists() {
-		// Try file existence: can we find the file in the file system?
 		try {
+// 基于 File 进行判断
 			return getFile().exists();
-		}
-		catch (IOException ex) {
-			// Fall back to stream existence: can we open the stream?
+		} catch (IOException ex) {
+// Fall back to stream existence: can we open the stream?
+// 基于 InputStream 进行判断
 			try {
-				getInputStream().close();
+				InputStream is = getInputStream();
+				is.close();
 				return true;
-			}
-			catch (Throwable isEx) {
+			} catch (Throwable isEx) {
 				return false;
 			}
 		}
 	}
 
 	/**
-	 * This implementation always returns {@code true} for a resource
-	 * that {@link #exists() exists} (revised as of 5.1).
+	 * 直接返回true，表示可读
 	 */
 	@Override
 	public boolean isReadable() {
-		return exists();
+		return true;
 	}
 
 	/**
-	 * This implementation always returns {@code false}.
+	 * 直接返回 false，表示未被打开
 	 */
 	@Override
 	public boolean isOpen() {
@@ -84,7 +81,7 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation always returns {@code false}.
+	 * 直接返回false，表示不为 File
 	 */
 	@Override
 	public boolean isFile() {
@@ -92,32 +89,29 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation throws a FileNotFoundException, assuming
-	 * that the resource cannot be resolved to a URL.
+	 * 抛出 FileNotFoundException 异常，交给子类实现
 	 */
 	@Override
 	public URL getURL() throws IOException {
 		throw new FileNotFoundException(getDescription() + " cannot be resolved to URL");
+
 	}
 
 	/**
-	 * This implementation builds a URI based on the URL returned
-	 * by {@link #getURL()}.
+	 * 基于 getURL() 返回的 URL 构建 URI
 	 */
 	@Override
 	public URI getURI() throws IOException {
 		URL url = getURL();
 		try {
 			return ResourceUtils.toURI(url);
-		}
-		catch (URISyntaxException ex) {
+		} catch (URISyntaxException ex) {
 			throw new NestedIOException("Invalid URI [" + url + "]", ex);
 		}
 	}
 
 	/**
-	 * This implementation throws a FileNotFoundException, assuming
-	 * that the resource cannot be resolved to an absolute file path.
+	 * 抛出 FileNotFoundException 异常，交给子类实现
 	 */
 	@Override
 	public File getFile() throws IOException {
@@ -125,10 +119,7 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation returns {@link Channels#newChannel(InputStream)}
-	 * with the result of {@link #getInputStream()}.
-	 * <p>This is the same as in {@link Resource}'s corresponding default method
-	 * but mirrored here for efficient JVM-level dispatching in a class hierarchy.
+	 * 根据 getInputStream() 的返回结果构建 ReadableByteChannel
 	 */
 	@Override
 	public ReadableByteChannel readableChannel() throws IOException {
@@ -136,63 +127,48 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation reads the entire InputStream to calculate the
-	 * content length. Subclasses will almost always be able to provide
-	 * a more optimal version of this, e.g. checking a File length.
-	 * @see #getInputStream()
+	 * 获取资源的长度
+	 * <p>
+	 * 这个资源内容长度实际就是资源的字节长度，通过全部读取一遍来判断
 	 */
 	@Override
 	public long contentLength() throws IOException {
 		InputStream is = getInputStream();
 		try {
 			long size = 0;
-			byte[] buf = new byte[256];
+			byte[] buf = new byte[255]; // 每次最多读取 255 字节
 			int read;
 			while ((read = is.read(buf)) != -1) {
 				size += read;
 			}
 			return size;
-		}
-		finally {
+		} finally {
 			try {
 				is.close();
-			}
-			catch (IOException ex) {
+			} catch (IOException ex) {
 			}
 		}
 	}
 
 	/**
-	 * This implementation checks the timestamp of the underlying File,
-	 * if available.
-	 * @see #getFileForLastModifiedCheck()
+	 * 返回资源最后的修改时间
 	 */
 	@Override
 	public long lastModified() throws IOException {
-		File fileToCheck = getFileForLastModifiedCheck();
-		long lastModified = fileToCheck.lastModified();
-		if (lastModified == 0L && !fileToCheck.exists()) {
+		long lastModified = getFileForLastModifiedCheck().lastModified();
+		if (lastModified == 0L) {
 			throw new FileNotFoundException(getDescription() +
-					" cannot be resolved in the file system for checking its last-modified timestamp");
+					" cannot be resolved in the file system for resolving its last-modified timestamp");
 		}
 		return lastModified;
 	}
 
-	/**
-	 * Determine the File to use for timestamp checking.
-	 * <p>The default implementation delegates to {@link #getFile()}.
-	 * @return the File to use for timestamp checking (never {@code null})
-	 * @throws FileNotFoundException if the resource cannot be resolved as
-	 * an absolute file path, i.e. is not available in a file system
-	 * @throws IOException in case of general resolution/reading failures
-	 */
 	protected File getFileForLastModifiedCheck() throws IOException {
 		return getFile();
 	}
 
 	/**
-	 * This implementation throws a FileNotFoundException, assuming
-	 * that relative resources cannot be created for this resource.
+	 * 抛出 FileNotFoundException 异常，交给子类实现
 	 */
 	@Override
 	public Resource createRelative(String relativePath) throws IOException {
@@ -200,8 +176,7 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation always returns {@code null},
-	 * assuming that this resource type does not have a filename.
+	 * 获取资源名称，默认返回 null ，交给子类实现
 	 */
 	@Override
 	@Nullable
@@ -209,33 +184,23 @@ public abstract class AbstractResource implements Resource {
 		return null;
 	}
 
-
 	/**
-	 * This implementation compares description strings.
-	 * @see #getDescription()
-	 */
-	@Override
-	public boolean equals(Object other) {
-		return (this == other || (other instanceof Resource &&
-				((Resource) other).getDescription().equals(getDescription())));
-	}
-
-	/**
-	 * This implementation returns the description's hash code.
-	 * @see #getDescription()
-	 */
-	@Override
-	public int hashCode() {
-		return getDescription().hashCode();
-	}
-
-	/**
-	 * This implementation returns the description of this resource.
-	 * @see #getDescription()
+	 * 返回资源的描述
 	 */
 	@Override
 	public String toString() {
 		return getDescription();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj == this ||
+				(obj instanceof Resource && ((Resource) obj).getDescription().equals(getDescription())));
+	}
+
+	@Override
+	public int hashCode() {
+		return getDescription().hashCode();
 	}
 
 }
--- spring-core/src/main/java/org/springframework/core/io/DefaultResourceLoader.java
+++ spring-core/src/main/java/org/springframework/core/io/DefaultResourceLoader.java
@@ -144,27 +144,28 @@ public class DefaultResourceLoader implements ResourceLoader {
 	public Resource getResource(String location) {
 		Assert.notNull(location, "Location must not be null");
 
+// 首先，通过 ProtocolResolver 来加载资源
 		for (ProtocolResolver protocolResolver : this.protocolResolvers) {
 			Resource resource = protocolResolver.resolve(location, this);
 			if (resource != null) {
 				return resource;
 			}
 		}
-
+// 其次，以 / 开头，返回 ClassPathContextResource 类型的资源
 		if (location.startsWith("/")) {
 			return getResourceByPath(location);
-		}
-		else if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+// 再次，以 classpath: 开头，返回 ClassPathResource 类型的资源
+		} else if (location.startsWith(CLASSPATH_URL_PREFIX)) {
 			return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()), getClassLoader());
-		}
-		else {
+// 然后，根据是否为文件 URL ，是则返回 FileUrlResource 类型的资源，否则返回 UrlResource 类型的资源
+		} else {
 			try {
-				// Try to parse the location as a URL...
+// Try to parse the location as a URL...
 				URL url = new URL(location);
 				return (ResourceUtils.isFileURL(url) ? new FileUrlResource(url) : new UrlResource(url));
-			}
-			catch (MalformedURLException ex) {
-				// No URL -> resolve as resource path.
+			} catch (MalformedURLException ex) {
+// 最后，返回 ClassPathContextResource 类型的资源
+// No URL -> resolve as resource path.
 				return getResourceByPath(location);
 			}
 		}
